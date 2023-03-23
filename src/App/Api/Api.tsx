1dca3115331f8f3bc9c0fe92a2f4dba2c078e3fa
Sebastiao Lutonda <lutonda@gmail.com>
2023-03-23 18:01:47 +0100

    implemented services signature

--- src/App/Api/Api.tsx
+++ src/App/Api/Api.tsx
@@ -3,43 +3,125 @@ import { useMemo, useState } from "react"
 
 type ApiParamsType = {
     service: any,
-    params: any,
-    id: string,
+    params?: any,
+    id?: string,
 }
 export const Api = ({ service, id, params }: ApiParamsType) => {
     const [data, setData] = useState<any>()
     const [isLoading, setLoading] = useState<boolean>()
     const [error, setError] = useState<any>()
-    const { endpoint, method } = service;
+    const { endpoint, method }: any = service;
+    const headers = getHeaders()
 
-    useMemo(() => {
-
-        const fetchGet = async () => {
+    const resolver: any = {
+        post: async ({ form }: any) => {
             setLoading(true)
             try {
-
-                let url = [endpoint];
-                if (id) {
-                    url.push(id)
-                }
-                const { response }: any = await axios.get(url.join('/'));
-
-                setData(response)
-                setLoading(false)
-
-            } catch (error) {
-                setError(error)
+                const { data }= await axios.post(endpoint, form, { headers });
+                console.log('👉 Returned data:', data);
+                //toast.success('Registo feito com sucesso');
+                setData(data)
+                setError(null)
+            } catch (e: any) {
+                console.log(`😱 Axios request failed: ${e}`);
+                setError(e)
+                //toast.error('😱 Erro: ' + JSON.stringify(e?.response?.data));
+                return e;
             }
+            setLoading(false)
+        }, get: async ({ id, params }: any) => {
+            setLoading(true)
+            let url = endpoint
+            if (id) {
+                url = `${url}/${id}`
+            }
+
+            if (params) {
+                const query = new URLSearchParams(params).toString();
+                url = `${url}/?${query}`;
+
+
+            }
+
+            try {
+                const response = await axios.get(url, { headers });
+                console.log('👉 Returned data:', response);
+                setData(response)
+                setError(null)
+            } catch (e: any) {
+                console.log(`😱 Axios request failed: ${e}`);
+                setError(e)
+                //toast.error('😱 Erro: ' + JSON.stringify(e?.response?.data));
+                return e;
+            }
+            setLoading(false)
+        }, drop: async ({ id }: any) => {
+            setLoading(true)
+            let url = endpoint
+            if (id) {
+                url = `${url}/${id}`
+            }
+
+            try {
+                const response = await axios.delete(url, { headers });
+                console.log('👉 Returned data:', response);
+                setData(response)
+                setError(null)
+            } catch (e: any) {
+                console.log(`😱 Axios request failed: ${e}`);
+                setError(e)
+                //toast.error('😱 Erro: ' + JSON.stringify(e?.response?.data));
+                return e;
+            }
+            setLoading(false)
+        }, put: async ({ id, form }: any) => {
+            setLoading(true)
+            let url = `${endpoint}/${data?.id ?? id}`
+
+            try {
+                const response = await axios.put(url, data, { headers });
+                console.log('👉 Returned data:', response);
+                setData(response)
+                setError(null)
+            } catch (e: any) {
+                console.log(`😱 Axios request failed: ${e}`);
+                setError(e)
+                //toast.error('😱 Erro: ' + JSON.stringify(e?.response?.data));
+                return e;
+            }
+            setLoading(false)
         }
+    }
 
+    const resolve = resolver[method.toLowerCase()]
+
+    useMemo(() => {
         if (method === 'GET')
-
-            fetchGet()
-
+            resolver.get({ id, params })
     }, [data, id])
 
-
     return {
-        data, isLoading, error
+        data, isLoading, error, resolve
     }
 }
+
+
+
+export const getHeaders = () => {
+    const token = '';//storage.get('token');
+
+    let headers: any = {
+        "withCredentials": true,
+        "Content-type": "application/json",
+    }
+    headers["Authorization"] = `Bearer ${token}`;
+    return headers;
+};
+(() => {
+    axios.defaults.baseURL = 'http://192.168.100.44:9800/api/v1/'/**///process.env.REACT_APP_BASE_URL_API;
+
+    axios.defaults.headers.common["apikey"] = "process.env.REACT_APP_BASE_API_KEY ?? '3265'";
+
+})()
+
+const headers = getHeaders() 
