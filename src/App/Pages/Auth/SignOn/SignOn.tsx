da893132122a23d4ad119334b13fe78d2b22a32c
= <=>
2023-03-23 18:05:09 +0100

    signon testing

--- src/App/Pages/Auth/SignOn/SignOn.tsx
+++ src/App/Pages/Auth/SignOn/SignOn.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { signOnDataType } from "./SignOnInterface";
+import { SignOnComponent } from "./SignOnFormComponet";
 
 export const SignOn = () => {
+ 
+
   return (
     <>
       <div className="col-md-6">
@@ -21,7 +25,6 @@ export const SignOn = () => {
                 project for free.
               </p>
               <div className="d-flex align-items-center">
-                
                 <p className="font-weight-bold text-white text-sm mb-0 ms-2">
                   Join 2.5M+ users
                 </p>
@@ -42,79 +45,14 @@ export const SignOn = () => {
             <p className="mb-0">Nice to meet you! Please enter your details.</p>
           </div>
           <div className="card-body">
-            <form role="form">
-              <label>Name</label>
-              <div className="mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter your name"
-                  aria-label="Name"
-                  aria-describedby="name-addon"
-                />
-              </div>
-              <label>Email Address</label>
-              <div className="mb-3">
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Enter your email address"
-                  aria-label="Email"
-                  aria-describedby="email-addon"
-                />
-              </div>
-              <label>Password</label>
-              <div className="mb-3">
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Create a password"
-                  aria-label="Password"
-                  aria-describedby="password-addon"
-                />
-              </div>
-              <div className="form-check form-check-info text-left mb-0">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label
-                  className="font-weight-normal text-dark mb-0"
-                  htmlFor="flexCheckDefault"
-                >
-                  I agree the{" "}
-                  <a href="javascript:;" className="text-dark font-weight-bold">
-                    Terms and Conditions
-                  </a>
-                  .
-                </label>
-              </div>
-              <div className="text-center">
-                <button type="button" className="btn btn-dark w-100 mt-4 mb-3">
-                  Sign up
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-white btn-icon w-100 mb-3"
-                >
-                  <span className="btn-inner--icon me-1">
-                    <img
-                      className="w-5"
-                      src="../assets/img/logos/google-logo.svg"
-                      alt="google-logo"
-                    />
-                  </span>
-                  <span className="btn-inner--text">Sign up with Google</span>
-                </button>
-              </div>
-            </form>
+
+            <SignOnComponent />
+
           </div>
           <div className="card-footer text-center pt-0 px-lg-2 px-1">
             <p className="mb-4 text-xs mx-auto">
               Already have an account?
-              <Link to="/auth/signin"  className="text-dark font-weight-bold">
+              <Link to="/auth/signin" className="text-dark font-weight-bold">
                 Sign in
               </Link>
             </p>
