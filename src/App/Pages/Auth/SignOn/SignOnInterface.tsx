18fccd70348802b333ab37f70b611eaf845ae9ba
= <=>
2023-03-23 18:04:56 +0100

    signon interfaces added

---
+++ src/App/Pages/Auth/SignOn/SignOnInterface.tsx
@@ -0,0 +1,11 @@
+export interface signOnDataType {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface IFormInput {
+  name: string;
+  email: string;
+  password: string;
+}
