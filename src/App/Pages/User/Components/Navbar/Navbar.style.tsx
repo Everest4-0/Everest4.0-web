1946c0a838ec94422e50ccc8251634937a0ebc38
= <=>
2023-03-24 10:15:38 +0100

    User navbar fixed

--- src/App/Pages/User/Components/Navbar/Navbar.style.tsx
+++ src/App/Pages/User/Components/Navbar/Navbar.style.tsx
@@ -1,24 +1,37 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    background: #0f172A;
-    
-`
+  background: #0f172a;
+`;
 
 export const Menu = styled.ul`
+  margin: 0;
+  padding: 10px;
+  color: #fff;
 
-margin: 0;
-padding: 10px;
-color: #fff;
-
-.logo{
+  .logo {
     font-size: 1.2rem;
-    margin-right: 25px;
-}
-` 
+    margin-right: 35px;
+  }
+
+  .link {
+    text-align: center;
+    width: 80px;
+  }
+  .link.link-active {
+    padding: 10px;
+    background-color: #1e293b;
+    border-radius: 0.25rem;
+  }
+`;
 
 export const Items = styled.li`
-    margin: 10px;
+  margin: 10px;
+`;
 
-`
 
+export const SearchBox = styled.input`
+  :focus {
+    color: #fff;
+  }
+`;
