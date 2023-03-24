f2ebcee02ef1c8c576dee78fe6fbc7ccbe470635
= <=>
2023-03-24 10:16:04 +0100

    user layout updated

--- src/App/Layout/UserLayout/UserLayout.tsx
+++ src/App/Layout/UserLayout/UserLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../../Pages/User/Components/Footer/Footer";
 import Navbar from "../../Pages/User/Components/Navbar/Navbar";
 
 export default function UserLayout({ children }: any) {
@@ -7,6 +8,7 @@ export default function UserLayout({ children }: any) {
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
